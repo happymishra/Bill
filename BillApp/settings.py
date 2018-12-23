5cf52639dcbd8acb0cb570c4d74c80d2aa6b5499
Rupesh Mishra <rupesh.mishra@visiblealpha.local>
2018-12-23 20:15:35 +0530

    Aadded statc file

--- BillApp/settings.py
+++ BillApp/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# PROJECT_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # # Add these new lines
 # STATICFILES_DIRS = (
