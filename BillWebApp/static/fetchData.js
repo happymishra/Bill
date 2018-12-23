3d859a7c4767c4db298a2e2798ecc18baa47fb1c
Rupesh Mishra <rupesh.mishra@visiblealpha.local>
2018-12-23 21:21:21 +0530

    static files chnges

--- BillWebApp/static/fetchData.js
+++ BillWebApp/static/fetchData.js
@@ -3,7 +3,7 @@ function getAllBillDetails() {
         type: 'GET',
         dataType: 'json',
         async: false,
-        url: 'http://127.0.0.1:8100/api/getBillDetails',
+        url: '/api/getBillDetails',
         contentType: 'application/json',
         success: function (data) {
             var dataAdapter = new $.jqx.dataAdapter(prepareDataForGrid(data.data));
